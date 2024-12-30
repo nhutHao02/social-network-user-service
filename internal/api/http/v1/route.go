9d73421caba41b9f94ba4df6084fe0d983b7d273
nhutHao02 <trannhuthao091@gmail.com>
2024-12-30 08:41:58 +0700

    update cors

--- internal/api/http/v1/route.go
+++ internal/api/http/v1/route.go
@@ -5,6 +5,7 @@ import (
 	"github.com/nhutHao02/social-network-common-service/middleware"
 	"github.com/nhutHao02/social-network-common-service/utils/logger"
 
+	"github.com/gin-contrib/cors"
 	_ "github.com/nhutHao02/social-network-user-service/docs"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
@@ -14,6 +15,13 @@ func MapRoutes(
 	router *gin.Engine,
 	userHandler *UserHandler,
 ) {
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 	v1 := router.Group("/api/v1")
 	{
 		v1.Use(middleware.JwtAuthMiddleware(logger.GetDefaultLogger()))
@@ -30,7 +38,7 @@ func MapRoutes(
 		vUser.POST("un-follow", userHandler.UnFollow)
 	}
 
-	v1Guest := router.Group("ap1/v1/guest")
+	v1Guest := router.Group("api/v1/guest")
 	{
 		v1Guest.POST("/login", userHandler.Login)
 		v1Guest.POST("/sign-up", userHandler.SignUp)
